2226bc2ab5d0393024fac6fd9e63b215c141e5ee
SentientCatBag <kumaribot@googlemail.com>
2017-04-18 18:15:50 +0100

    Update event_table.cpp

--- event_table.cpp
+++ event_table.cpp
@@ -2,15 +2,26 @@
 //not finished
 //each function does exactly what its name says :)
 
-void start_turn();
+void start_turn(){
+  game->turn++;
+  if (game->turn/2 == ){     //( == a whole number)
+    game->myturn = false;
+  }
+  else {
+    game->myturn = true;
+  }
+}
+
 void (*p_start_turn)();
 p_start_turn = & start_turn;
 
 void end_turn();
+
 void(*p_end_turn)();
 p_end_turn = & end_turn;
 
 void move_unit(unit->range); //probably more arguments to be added
+
 void (*p_move_unit) (unit->range);
 p_move_unit = & move_unit;
 

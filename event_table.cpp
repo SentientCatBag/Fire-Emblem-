3e089472793018ae6fbc4bf491efbfad5fe8fb37
SentientCatBag <kumaribot@googlemail.com>
2017-04-18 18:08:47 +0100

    Create event_table.cpp
    List of events (function pointers) and their corresponding functions

---
+++ event_table.cpp
@@ -0,0 +1,16 @@
+//events
+//not finished
+//each function does exactly what its name says :)
+
+void start_turn();
+void (*p_start_turn)();
+p_start_turn = & start_turn;
+
+void end_turn();
+void(*p_end_turn)();
+p_end_turn = & end_turn;
+
+void move_unit(unit->range); //probably more arguments to be added
+void (*p_move_unit) (unit->range);
+p_move_unit = & move_unit;
+
